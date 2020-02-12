e16baa8bdeb4792389dffab37793d488df6c540b
Sarah Wolf <sarahw@unity3d.com>
2020-02-12 15:32:09 -0800

    robot agent - WIP

---
+++ ArmRobot/Assets/Scripts/RobotAgent.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class RobotAgent : Agent
+{
+    public GameObject endEffector;
+    public GameObject cube;
+    public GameObject robot;
+
+    
+    RobotController robotController;
+    /*
+    TouchDetector touchDetector;
+    TablePositionRandomizer tablePositionRandomizer;
+        
+
+    void Start()
+    {
+        robotController = robot.GetComponent<RobotController>();
+        touchDetector = endEffector.GetComponent<TouchDetector>();
+        tablePositionRandomizer = cube.GetComponent<TablePositionRandomizer>();
+    }
+
+
+    // AGENT
+    */
+    public override void AgentReset()
+    {
+        /*
+        float[] defaultRotations = { 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f };
+        robotController.ForceJointsToRotations(defaultRotations);
+        touchDetector.hasTouchedTarget = false;
+        tablePositionRandomizer.Move();
+        */
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        /*
+        // current rotations
+        float[] rotations = robotController.GetCurrentJointRotations();
+        foreach (float rotation in rotations)
+        {
+            float rotationRadians = rotation * (Mathf.PI / 180.0f);
+            AddVectorObs(Mathf.Sin(rotationRadians));
+            AddVectorObs(Mathf.Cos(rotationRadians));
+        }
+
+        //cube position
+        Vector3 cubePosition = cube.transform.position - robot.transform.position;
+        AddVectorObs(cubePosition);
+
+        //end position
+        Vector3 endPosition = endEffector.transform.position - robot.transform.position;
+        AddVectorObs(endPosition);
+        */
+    }
+
+    public override void AgentAction(float[] vectorAction)
+    {
+        /*
+        // move
+        int jointIndex = (int)vectorAction[0];
+        int actionIndex = (int)vectorAction[1];
+        RotationDirection rotationDirection = AgentHelpers.ActionIndexToRotationDirection(actionIndex);
+        robotController.RotateJoint(jointIndex, rotationDirection);
+
+        // end episode if we touched the cube
+        if (touchDetector.hasTouchedTarget)
+        {
+            Done();
+        }
+
+        //reward
+        float distanceToCube = Vector3.Distance(endEffector.transform.position, cube.transform.position);
+        SetReward(-distanceToCube * 0.01f);
+        */
+    }
+
+
+
+    
+
+
+}
+
+
---
+++ ArmRobot/Assets/Scripts/TouchDetector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchDetector : MonoBehaviour
+{
+    public GameObject touchTarget;
+    public bool hasTouchedTarget = false;
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.gameObject.name == touchTarget.name)
+        {
+            Debug.Log("Touch Detected!");
+            hasTouchedTarget = true;
+        }
+    }
+}
