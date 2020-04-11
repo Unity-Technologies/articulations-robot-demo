f8ab76a33e450c78f2dbf50a476e6950ce7f4159
Sarah Wolf <sarahw@unity3d.com>
2020-04-11 15:10:46 -0700

    can move up and down

---
+++ ArmRobot/Assets/Scripts/GripperDemoManualInput.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GripperDemoManualInput : MonoBehaviour
+{
+    public GameObject hand;
+
+
+    void Update()
+    {
+        float input = Input.GetAxis("BigHandVertical");
+        BigHandState moveState = MoveStateForInput(input);
+        GripperDemoController controller = hand.GetComponent<GripperDemoController>();
+        controller.moveState = moveState;
+    }
+
+    BigHandState MoveStateForInput(float input)
+    {
+        if (input > 0)
+        {
+            return BigHandState.MovingUp;
+        }
+        else if (input < 0)
+        {
+            return BigHandState.MovingDown;
+        }
+        else
+        {
+            return BigHandState.Fixed;
+        }
+    }
+}
