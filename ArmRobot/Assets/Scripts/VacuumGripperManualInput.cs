ec5c5aa8983c266ac5ad0e47284a1b345516de6b
Sarah Wolf <sarahw@unity3d.com>
2020-04-15 16:47:37 -0700

    getting keyboard input

---
+++ ArmRobot/Assets/Scripts/VacuumGripperManualInput.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VacuumGripperManualInput : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Base");
+        float verticalInput = Input.GetAxis("Shoulder");
+
+        if (horizontalInput != 0)
+        {
+            Debug.Log("moving horizontally!");
+        }
+
+        if (verticalInput != 0)
+        {
+            Debug.Log("moving vertically!");
+        }
+    }
+}
