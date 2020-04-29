d67ad7fe7a8687f78b29ced709954358d250d15e
Sarah Wolf <sarahw@unity3d.com>
2020-04-29 13:25:33 -0700

    collecting references to all the bones

---
+++ ArmRobot/Assets/Scripts/CCDIK.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CCDIK : MonoBehaviour
+{
+    /*
+     * Must be placed on a game object with a non-root articulation body. 
+     */
+
+    public float sqrDistError = 0.01f;
+    public int maxIterationCount = 10;
+
+    List<Transform> bones = new List<Transform>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // collect bones
+        Transform currentBone = transform;
+        bool done = false;
+        while (!done)
+        {
+            bones.Add(currentBone);
+
+            Transform parent = currentBone.parent;
+            ArticulationBody parentArticulation = parent.gameObject.GetComponent<ArticulationBody>();
+
+            done = parentArticulation.isRoot;
+            currentBone = parent;
+        }
+
+        // read out bones to test
+        for (int i = 0; i < bones.Count; i++)
+        {
+            Transform bone = bones[i];
+            Debug.Log(bone.gameObject.name);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
