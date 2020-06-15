bf31390a09ee5cd46c7dc9a6d933d592b91a3bdd
Jonathan Leban <jonathan.leban@jonathan.leban-406M>
2020-06-15 15:35:19 -0700

    I removed the configurations joints and add some function inside ArticulationJointController

--- ArmRobot/Assets/Scripts/ArticulationJointController.cs
+++ ArmRobot/Assets/Scripts/ArticulationJointController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,29 +9,18 @@ public class ArticulationJointController : MonoBehaviour
 {
     public RotationDirection rotationState = RotationDirection.None;
     public float speed = 300.0f;
-    public float r;
 
-    private ConfigurableJoint joint;
-
-    Vector3 lastLocalPosition;
-    Quaternion lastLocalRotation;
-
+    private ArticulationBody articulation;
 
 
     // LIFE CYCLE
 
-    private void Awake()
-    {
-        joint = GetComponent<ConfigurableJoint>();
-    }
-
     void Start()
     {
-        lastLocalPosition = transform.localPosition;
-        lastLocalRotation = transform.localRotation;
+        articulation = GetComponent<ArticulationBody>();
     }
 
-    void FixedUpdate()
+    void FixedUpdate() 
     {
         if (rotationState != RotationDirection.None) {
             float rotationChange = (float)rotationState * speed * Time.fixedDeltaTime;
@@ -38,47 +28,6 @@ public class ArticulationJointController : MonoBehaviour
             RotateTo(rotationGoal);
         }
 
-        UpdateLockState();
-        r = GetPrimaryAxisRotation();
-    }
-
-
-    // CONTROL
-
-    public void ForceToRotation(float primaryAxisRotation)
-    {
-        // set joint to given rotation
-        RotateTo(primaryAxisRotation);
-
-        // set transform to given rotation
-        float x = (joint.axis.x * primaryAxisRotation) + (1.0f - joint.axis.x) * transform.localRotation.eulerAngles.x;
-        float y = (joint.axis.y * primaryAxisRotation) + (1.0f - joint.axis.y) * transform.localRotation.eulerAngles.y;
-        float z = (joint.axis.z * primaryAxisRotation) + (1.0f - joint.axis.z) * transform.localRotation.eulerAngles.z;
-        transform.localRotation = Quaternion.Euler(x, y, z);
-    }
-
-
-    // READ
-
-    public float GetPrimaryAxisRotation()
-    {
-        Vector3 currentRotation = transform.localRotation.eulerAngles;
-        float primaryAxisRotation = currentRotation.x * joint.axis.x + currentRotation.y * joint.axis.y + currentRotation.z * joint.axis.z;
-        return primaryAxisRotation;
-    }
-
-
-    // RIGIDITY HELPERS
-
-    void UpdateLockState()
-    {
-        transform.localPosition = lastLocalPosition;
-
-        // lock all but the axes of rotation
-        float x = (joint.axis.x * transform.localRotation.eulerAngles.x) + ((1.0f - joint.axis.x) * lastLocalRotation.eulerAngles.x);
-        float y = (joint.axis.y * transform.localRotation.eulerAngles.y) + ((1.0f - joint.axis.y) * lastLocalRotation.eulerAngles.y);
-        float z = (joint.axis.z * transform.localRotation.eulerAngles.z) + ((1.0f - joint.axis.z) * lastLocalRotation.eulerAngles.z);
-        transform.localRotation = Quaternion.Euler(x, y, z);
 
     }
 
@@ -87,17 +36,35 @@ public class ArticulationJointController : MonoBehaviour
 
     float CurrentPrimaryAxisRotation()
     {
-        Vector3 currentRotation = transform.localRotation.eulerAngles;
-        float rotation = joint.axis.x * currentRotation.x + joint.axis.y * currentRotation.y + joint.axis.z * currentRotation.z;
-        return rotation;
+        float currentRotationRads = articulation.jointPosition[0];
+        float currentRotation = Mathf.Rad2Deg * currentRotationRads;
+        return currentRotation;
     }
 
     void RotateTo(float primaryAxisRotation)
     {
-        Vector3 goalRotation = new Vector3(primaryAxisRotation, 0.0f, 0.0f);
-        joint.targetRotation = Quaternion.Inverse(Quaternion.Euler(goalRotation));
+        var drive = articulation.xDrive;
+        drive.target = primaryAxisRotation;
+        articulation.xDrive = drive;
     }
 
+    public void ForceToRotation(float primaryAxisRotation)
+    {
+        // set joint to given rotation
+        RotateTo(primaryAxisRotation);
 
+        // set transform to given rotation
+        float x = (articulation.jointPosition[0] * primaryAxisRotation) + (1.0f - articulation.jointPosition[0]) * transform.localRotation.eulerAngles.x;
+        float y = (articulation.jointPosition[1] * primaryAxisRotation) + (1.0f - articulation.jointPosition[1]) * transform.localRotation.eulerAngles.y;
+        float z = (articulation.jointPosition[2] * primaryAxisRotation) + (1.0f - articulation.jointPosition[2]) * transform.localRotation.eulerAngles.z;
+        transform.localRotation = Quaternion.Euler(x, y, z);
+    }
+
+    public float GetPrimaryAxisRotation()
+    {
+        Vector3 currentRotation = transform.localRotation.eulerAngles;
+        float primaryAxisRotation = currentRotation.x * articulation.jointPosition[0] + currentRotation.y * articulation.jointPosition[1] + currentRotation.z * articulation.jointPosition[2];
+        return primaryAxisRotation;
+    }
 
 }
